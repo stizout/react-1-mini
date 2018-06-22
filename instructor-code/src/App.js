fdd936c7bc191f5aaa6657a44f5f0112a1b45668
Alex Stout <stizout@gmail.com>
2018-06-22 14:38:44 -0700

    create input and friends array

--- instructor-code/src/App.js
+++ instructor-code/src/App.js
@@ -2,13 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      picture: 'alksdjfalksfj',
+      name: 'Tyler',
+      friends: [{
+        name: 'My 404 friend',
+        picture: 'https://http.cat/404'
+      }, {
+        name: 'My chill cat with the munchies',
+        picture: 'https://http.cat/420'
+      }]
+    };
+  }
+
+  changeName(name) {
+    this.setState({
+      name: name,
+    })
+  }
+
+  changePicture = (event) => {
+    this.setState({
+      picture: event.target.value,
+    })
+  }
+  
   render() {
     return (
-      <div>lkjsdlkfjasdklfj</div>
+      <div>
+        Name: <input value={this.state.name} onChange={(event) => this.changeName(event.target.value)} />
+        Picture: <input value={this.state.picture} onChange={(event) => this.changePicture(event)} />
+        <div>
+          {this.state.friends.map(friend => {
+            console.log('friend', friend);
+            return (
+              <div>
+                <img src={friend.picture}/>
+                <b>{friend.name}</b>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     );
   }
 }
 
 export default App;
-
-export const myString = 'something';
--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      picture: '',
+      name: '',
+      friends: [{
+        name: "Alex",
+        picture: "https://www.swapaw.com/blog/wp-content/uploads/2017/07/funny-dog-8-e1500643440478.jpg"
+      },{
+        name: "Cher",
+        picture: "https://www.askideas.com/wp-content/uploads/2016/11/Cute-Funny-Dog-With-Funny-Hairstyle.jpg"
+      }]
+    }
+  }
+
+  changeName(event) {
+    const name = event.target.value
+    this.setState(() => {
+      return {
+        name: name
+      }
+    })
+  }
+
+  updatePicture(event) {
+    const picture = event.target.value
+    this.setState(() => {
+      return {
+        picture: picture
+      }
+    })
+  }
+
+
   render() {
     return (
       <div>
-        
+        Name: <input value={this.state.name} onChange={(event) => {
+          this.changeName(event)
+        }}/>
+        Picture: <input value={this.state.picture} onChange={(event) => {
+          this.updatePicture(event)
+        }} />
+        <div>
+          {this.state.friends.map((friend) => {
+            return (
+              <div>
+                <h1>{friend.name}</h1>
+                <img src={friend.picture} />
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
 }
+
+
 
 export default App;
